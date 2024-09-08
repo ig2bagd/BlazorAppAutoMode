60c70025aa3ff7b930f3c7e86d945e8f376987b8
steviep <ig2bagd@yahoo.com>
2024-09-07 22:15:30 -0700

    Use Options Pattern

---
+++ BlazorAppAutoMode/BlazorAppAutoMode/AppOptions.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorAppAutoMode;
+
+public class AppOptions
+{
+    public const string ConfigurationSection = "MySection";
+
+    [Required, Url]
+    public string BaseAddress { get; set; } = string.Empty;
+}
