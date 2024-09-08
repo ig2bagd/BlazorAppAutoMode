5b6f5684cd7166a6553a49d0abe79eba30a4fb14
steviep <ig2bagd@yahoo.com>
2024-09-07 20:54:31 -0700

    Correct use of typed client

--- BlazorAppAutoMode/BlazorAppAutoMode.Client/Services/DataService.cs
+++ BlazorAppAutoMode/BlazorAppAutoMode.Client/Services/DataService.cs
@@ -8,7 +8,8 @@ public class DataService(HttpClient httpClient) : IDataService
     private readonly HttpClient _httpClient = httpClient;
     public async Task<OpenMeteoResult?> GetData()
     {
-        var apiResult = await _httpClient.GetAsync("api/gwd");
-        return apiResult.IsSuccessStatusCode ? await apiResult.Content.ReadFromJsonAsync<OpenMeteoResult>() : null;
+        //var apiResult = await _httpClient.GetAsync("api/gwd");
+        //return apiResult.IsSuccessStatusCode ? await apiResult.Content.ReadFromJsonAsync<OpenMeteoResult>() : null;
+        return await _httpClient.GetFromJsonAsync<OpenMeteoResult>("api/gwd");
     }
 }
